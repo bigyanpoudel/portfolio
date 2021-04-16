32ab212455e4a212e7a43fef537adbd03d811031
bigyan poudel <bgyanpoudel11@gmail.com>
2021-04-16 23:40:42 +0545

    sanity io

--- component/Contact.js
+++ component/Contact.js
@@ -1,19 +1,55 @@
-import React from 'react'
-
+import React,{useEffect,useState} from 'react'
+const defaultValue = {
+    name:'',
+    email:'',
+    subject:'',
+    message:''
+}
 const Contact = () => {
+    const [data,SetData] = useState(defaultValue);
+    const changeHandler = (e)=>{
+        const {name,value} = e.target;
+        SetData({...defaultValue,[name]:value});
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: data,
+    };
+
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        const submit =async()=>{
+            const res = await fetch(`${process.env.CLIENT_URL}/send/email`,requestOptions);
+            console.log(res);
+        }
+        submit();
+    }
     return (
         <div className="Contact" id="contact">
              <div className="header__title grey">Contact</div>
              <h2>Have a question or want to work together?</h2>
-             <form className="Contact__form">
+             <form className="Contact__form" onSubmit={submitHandler}>
                  <div className="Contact__form-group">
-                 <input type="text" name="name" placeholder="Name" required/>
+                 <input type="text" name="name" placeholder="Name" required 
+                 value={data.name}
+                 onChange={changeHandler}/>
                  </div>
                  <div className="Contact__form-group">
-                 <input type="email" name="email" placeholder="Email" required/>
+                 <input type="email" name="email" placeholder="Email" required 
+                 value={data.email}
+                 onChange={changeHandler}/>
                  </div>
                  <div className="Contact__form-group">
-                 <textarea rows={5} cols={10} name="message" placeholder="message"/>
+                 <input type='text' name="subject" placeholder="Subject" required 
+                 value={data.subject}
+                 onChange={changeHandler}/>
+                 </div>
+                 <div className="Contact__form-group">
+                 <textarea rows={5} cols={10} name="message" placeholder="message" 
+                 value={data.message}
+                 onChange={changeHandler}/>
 
                  </div>
 
--- server/server.js
+++ server/server.js
@@ -2,18 +2,17 @@ const next = require('next');
 const express = require('express');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handdle = app.getRequestHandler();
-
+const handle = app.getRequestHandler();
 app.prepare().then(() => {
     const server = express();
     const PORT = process.env.PORT || 3000;
     server.use(express.json());
 
     server.get('*', (req, res) => {
-        return handdle(req, res);
+        return handle(req, res);
     });
     server.listen(PORT, (err) => {
         if (err) throw err;
-        console.log(`server is running at ${PORT}`);
+        console.log(`server is running at port ${PORT}`);
     });
 });
